f7e82da425dca914b206cb45f5d8558da5fb711d
Torsten Krüger <torsten.kruger90@gmail.com>
2022-12-14 12:33:10 +0100

    Added empty myLeads array and changed let to const for inputBtn and inputEl

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-let inputBtn = document.querySelector("#input-btn");
+const inputBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
+let myLeads = [];
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button was indeed clicked!");
+    inputEl.textContent
 });
