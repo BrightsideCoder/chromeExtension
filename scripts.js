1baa4508b38829542f118c2af810b0c8dd2faf54
Torsten Krüger <torsten.kruger90@gmail.com>
2022-12-16 13:46:35 +0100

    Wrapped for loop into a renderLeads()

--- scripts.js
+++ scripts.js
@@ -5,7 +5,14 @@ let myLeads = [];
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
-    inputEl.value = "";
+    renderLeads();
 });
 
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>";
+    }
+
+    ulEl.innerHTML = "<ul>" + listItems + "</ul>";
+}
