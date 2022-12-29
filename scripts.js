f45651a8b242d5ecb1bacddead90470219b3985b
Torsten Krüger <torsten.kruger90@gmail.com>
2022-12-29 13:34:46 +0100

    Changed renderLeads() to render(); Added parameter to render() for 'future use'

--- scripts.js
+++ scripts.js
@@ -1,25 +1,18 @@
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn");
+const leadsFromStorage = JSON.parse( localStorage.getItem("myLeads") );
 let myLeads = [];
-
-let leadsFromStorage = JSON.parse( localStorage.getItem("myLeads") );
 
 if (leadsFromStorage) {
     myLeads = leadsFromStorage;
-    renderLeads();
+    render(myLeads);
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
     let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
                     <li>
                         <a href='#' target='_blank'>
@@ -30,3 +23,16 @@ function renderLeads() {
 
     ulEl.innerHTML = "<ul>" + listItems + "</ul>";
 }
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
+
+deleteBtn.addEventListener("dblclick", function() {
+    myLeads = [];
+    localStorage.clear();
+    render(myLeads);
+});
